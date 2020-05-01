30211663868397a650a101ce54057df35a5c5d6f
Paras Varshney <paras.varshney97@gmail.com>
2020-05-01 14:27:09 +0530

    updated model and build regressor

--- model.py
+++ model.py
@@ -1,7 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May  1 14:13:23 2020
+
+@author: paras
+"""
+
 # Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.externals import joblib 
 
 # Importing the training set
 dataset_train = pd.read_csv('Google_Stock_Price_Train.csv')
@@ -43,14 +51,15 @@ regressor.add(LSTM(units = 50, return_sequences = True))
 regressor.add(Dropout(0.2))
 regressor.add(LSTM(units = 50, return_sequences = True))
 regressor.add(Dropout(0.2))
-
 regressor.add(LSTM(units = 50))
 regressor.add(Dropout(0.2))
 
 regressor.add(Dense(units = 1))
 
 regressor.compile(optimizer = 'adam', loss = 'mean_squared_error')
-regressor.fit(X_train, y_train, epochs = 100, batch_size = 32)
+regressor.fit(X_train, y_train, epochs = 50, batch_size = 32)
+
+joblib.dump(regressor, "regressor.pkl")
 
 
 # Getting the real stock price of 2017
@@ -62,6 +71,7 @@ dataset_total = pd.concat((dataset_train['Open'], dataset_test['Open']), axis = 
 inputs = dataset_total[len(dataset_total) - len(dataset_test) - 60:].values
 inputs = inputs.reshape(-1,1)
 inputs = sc.transform(inputs)
+
 X_test = []
 for i in range(60, 80):
     X_test.append(inputs[i-60:i, 0])
